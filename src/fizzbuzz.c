211b364517cb2b0032276fe5d32284fd373aad0f
Clay Dowling <clayton.c.dowling@accenture.com>
2020-01-14 15:39:22 -0500

    First passing test

---
+++ src/fizzbuzz.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include "fizzbuzz.h"
+
+const char *fizzbuzz(int i)
+{
+    return "1";
+}
---
+++ test/test_fizzbuzz.c
@@ -0,0 +1,18 @@
+#include "unity_fixture.h"
+#include "fizzbuzz.h"
+
+TEST_GROUP(FizzBuzz);
+
+TEST_SETUP(FizzBuzz) {}
+
+TEST_TEAR_DOWN(FizzBuzz) {}
+
+TEST(FizzBuzz, GivenOne_ReturnsString1)
+{
+    TEST_ASSERT_EQUAL_STRING("1", fizzbuzz(1));
+}
+
+TEST_GROUP_RUNNER(FizzBuzz)
+{
+    RUN_TEST_CASE(FizzBuzz, GivenOne_ReturnsString1);
+}
