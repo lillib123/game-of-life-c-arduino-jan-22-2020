28c78e70655af73a04c3a97fcea0908dc249eac7
Clay Dowling <clayton.c.dowling@accenture.com>
2020-01-21 13:01:37 -0500

    Removed incorrect setup code

--- src/life.cpp
+++ src/life.cpp
@@ -18,15 +18,6 @@ void life_setup(void *matrix)
     uint16_t livecolor = led_color(127, 127, 127);
     uint16_t deadcolor = led_color(0, 0, 0);
 
-    for (uint8_t x = 0; x < BOARDX; ++x)
-    {
-        for (uint8_t y = 0; y < BOARDY; ++y)
-        {
-            v = rand() % 2;
-            // v = x % 2;
-            led_draw(x, y, v == 0 ? deadcolor : livecolor);
-        }
-    }
 }
 
 void life_loop(void *matrix)
